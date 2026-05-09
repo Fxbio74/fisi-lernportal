13d9f55db57ebfe17356ad54ec91f62c1aa1e0fd
Fabio <fgrossegehling@gmail.com>
2026-05-09 17:22:52 +0200

    Fix IP detection via own API endpoint

--- api/get-ip.js
+++ api/get-ip.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const ip =
-    req.headers["x-forwarded-for"]?.split(",")[0].trim() ||
+    req.headers["x-forwarded-for"]?.split(",")[0] ||
     req.headers["x-real-ip"] ||
     req.socket?.remoteAddress ||
     "unbekannt";
